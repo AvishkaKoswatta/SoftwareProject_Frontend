c9cb4423bdace189c9dd54407e10b74593c8fd12
Avishka <avishkakkoswaththa@gmail.com>
2024-04-06 13:55:44 +0530

    hggh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './components/Home'; // Import the 'Home' component
+import Login from './components/Login';
+import Register from './components/Register';
+import Medical from './components/Medical';
+import Search from './components/Search';
+import About from './components/About';
+import Information from './components/Information';
+import Example from './components/Example';
+import Mtemplate from './components/Mtemplate';
+import Appointment from './components/Appointment';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const isTesting = 1;
+
+  
+  
+  if(isTesting)return (
+
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/medical" element={<Medical />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/information" element={<Information />} />
+          <Route path="/example" element={<Example />} />
+          <Route path="/mtemplate" element={<Mtemplate />} />
+          <Route path="/appointment" element={<Appointment />} />
+          
+        </Routes>
+      </Router>
+    </div>
+
+  );
+  else return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          
+        </Routes>
+      </Router>
     </div>
   );
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 export default App;
